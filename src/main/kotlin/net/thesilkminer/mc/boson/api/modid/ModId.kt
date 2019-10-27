7bf989610cacc609d12eb8c73592d31476a39b55
TheSilkMiner <thesilkminer@outlook.com>
2019-10-27 10:43:28 +0100

    Mod IDs first pass: add basic ones
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

---
+++ src/main/kotlin/net/thesilkminer/mc/boson/api/modid/ModId.kt
@@ -0,0 +1,24 @@
+@file:JvmName("ModId")
+@file:Suppress("SpellCheckingInspection", "Unused")
+
+package net.thesilkminer.mc.boson.api.modid
+
+import net.thesilkminer.mc.boson.MOD_ID
+
+const val BOSON = MOD_ID
+const val BOSON_ASM = "$BOSON.asm"
+const val CRAFT_TWEAKER_2 = "crafttweaker"
+const val CRAFT_TWEAKER_GUI = "ctgui"
+const val ENERGY_IS_MATTER = "energysmatter"
+const val ENERGY_IS_MATTER_CORE = "$ENERGY_IS_MATTER.asm"
+const val FERMION = "fermion"
+const val FERMION_ASM = "$FERMION.asm"
+const val FERMION_ASM_SERVICE = "$FERMION_ASM.service"
+const val FORGELIN = "forgelin"
+const val HWYLA = "waila"
+const val IN_GAME_WIKI = "igwmod"
+const val JUST_ENOUGH_ITEMS = "jei"
+const val PATCHOULI = "patchouli"
+const val TESLA = "tesla"
+const val THE_ONE_PROBE = "theoneprobe"
+
