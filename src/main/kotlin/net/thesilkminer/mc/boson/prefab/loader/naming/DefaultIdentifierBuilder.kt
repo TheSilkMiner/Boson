d133848e1d5487ddeebfbb902ddea345216813e4
TheSilkMiner <thesilkminer@outlook.com>
2019-11-27 09:59:33 +0100

    Fix name containing file extension
    While not wrong, this is surely not correct either
    
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/kotlin/net/thesilkminer/mc/boson/prefab/loader/naming/DefaultIdentifierBuilder.kt
+++ src/main/kotlin/net/thesilkminer/mc/boson/prefab/loader/naming/DefaultIdentifierBuilder.kt
@@ -6,11 +6,12 @@ import net.thesilkminer.mc.boson.api.loader.Context
 import net.thesilkminer.mc.boson.api.loader.IdentifierBuilder
 import net.thesilkminer.mc.boson.api.loader.Location
 import net.thesilkminer.mc.boson.prefab.loader.modIdContextKey
+import org.apache.commons.io.FilenameUtils
 
-class DefaultIdentifierBuilder : IdentifierBuilder {
+class DefaultIdentifierBuilder(private val removeExtension: Boolean = false) : IdentifierBuilder {
     override fun makeIdentifier(location: Location, globalContext: Context?, phaseContext: Context?) =
             bosonApi.constructNameSpacedString(
                     nameSpace = location.additionalContext?.get(modIdContextKey) ?: Loader.instance().activeModContainer()?.modId,
-                    path = location.path.toString().replace(Regex("\\\\"), "/")
+                    path = (if (this.removeExtension) FilenameUtils.removeExtension(location.path.toString()) else location.path.toString()).replace(Regex("\\\\"), "/")
             )
 }
