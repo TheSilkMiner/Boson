31f385eb054372b5c6c733ce78cc0c876e6159f9
TheSilkMiner <thesilkminer@outlook.com>
2020-02-01 11:16:58 +0100

    First draft for Boson compatibility providers
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

---
+++ src/main/kotlin/net/thesilkminer/mc/boson/compatibility/BosonCompatibilityProvider.kt
@@ -0,0 +1,5 @@
+package net.thesilkminer.mc.boson.compatibility
+
+import net.thesilkminer.mc.boson.api.compatibility.CompatibilityProvider
+
+interface BosonCompatibilityProvider : CompatibilityProvider
