e53841e0a47d1f72725f75ac4f1468025fd4daf0
TheSilkMiner <thesilkminer@outlook.com>
2020-02-19 10:41:03 +0100

    Allow converting from ZS types to native ones and viceversa
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

---
+++ src/main/kotlin/net/thesilkminer/mc/boson/compatibility/crafttweaker/ConversionUtilities.kt
@@ -0,0 +1,23 @@
+@file:JvmName("WrapUnwrap")
+
+package net.thesilkminer.mc.boson.compatibility.crafttweaker
+
+import net.thesilkminer.mc.boson.api.id.NameSpacedString
+import net.thesilkminer.mc.boson.api.tag.Tag
+import net.thesilkminer.mc.boson.api.tag.TagType
+import net.thesilkminer.mc.boson.compatibility.crafttweaker.naming.ZenNameSpacedString
+import net.thesilkminer.mc.boson.compatibility.crafttweaker.tag.ZenTag
+import net.thesilkminer.mc.boson.compatibility.crafttweaker.tag.ZenTagType
+import net.thesilkminer.mc.boson.compatibility.crafttweaker.zenscriptx.sequence.ZenSequence
+
+fun NameSpacedString.toZen() = ZenNameSpacedString(this.nameSpace, this.path)
+fun ZenNameSpacedString.toNative() = NameSpacedString(this.nameSpace, this.path)
+
+fun <T : Any> Tag<T>.toZen() = ZenTag(this)
+fun <T : Any> ZenTag<T>.toNative() = this.tag
+
+fun <T : Any> TagType<T>.toZen() = ZenTagType(this)
+fun <T : Any> ZenTagType<T>.toNative() = this.tagType
+
+fun <T> Sequence<T>.toZen() = ZenSequence(this)
+fun <T> ZenSequence<T>.toNative() = this.sequence
