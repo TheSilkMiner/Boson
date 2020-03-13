22775703e87144e0098f63a716de18b3472e1c66
TheSilkMiner <thesilkminer@outlook.com>
2020-03-13 13:45:39 +0100

    Removed the ability of using `ZenTagIngredient` as an `IOreDictEntry`
    Not because it didn't make sense (I still believe it does), but because
    tag manipulation can be done in `#loader tags` and because otherwise
    CraftTweaker considered us as an `OreDictionaryIngredient`, which is
    inherently wrong: `VanillaIngredient` is what we want, so that
    everything is deferred to us
    
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/kotlin/net/thesilkminer/mc/boson/compatibility/crafttweaker/tag/ZenTagIngredient.kt
+++ src/main/kotlin/net/thesilkminer/mc/boson/compatibility/crafttweaker/tag/ZenTagIngredient.kt
@@ -1,6 +1,5 @@
 package net.thesilkminer.mc.boson.compatibility.crafttweaker.tag
 
-import crafttweaker.CraftTweakerAPI
 import crafttweaker.annotations.ZenRegister
 import crafttweaker.api.item.IIngredient
 import crafttweaker.api.item.IItemCondition
@@ -10,11 +9,9 @@ import crafttweaker.api.item.IItemTransformerNew
 import crafttweaker.api.item.IngredientOr
 import crafttweaker.api.item.IngredientStack
 import crafttweaker.api.liquid.ILiquidStack
-import crafttweaker.api.oredict.IOreDictEntry
 import crafttweaker.api.player.IPlayer
 import net.minecraftforge.oredict.OreDictionary
 import net.thesilkminer.mc.boson.compatibility.crafttweaker.naming.ZenNameSpacedString
-import net.thesilkminer.mc.boson.compatibility.crafttweaker.toNativeStack
 import net.thesilkminer.mc.boson.compatibility.crafttweaker.toZen
 import net.thesilkminer.mc.boson.mod.common.recipe.TagIngredient
 import stanhebben.zenscript.annotations.ZenClass
@@ -23,7 +20,7 @@ import stanhebben.zenscript.annotations.ZenGetter
 @ZenClass("net.thesilkminer.mc.boson.zen.tag.TagIngredient")
 @ZenRegister
 class ZenTagIngredient(val tagIngredient: TagIngredient, private val mark: String? = null, private val conditions: List<IItemCondition> = listOf(),
-                       private val newTransformers: List<IItemTransformerNew> = listOf(), private val transformers: List<IItemTransformer> = listOf()) : IIngredient/*, IOreDictEntry*/ {
+                       private val newTransformers: List<IItemTransformerNew> = listOf(), private val transformers: List<IItemTransformer> = listOf()) : IIngredient {
 
     val tagName: ZenNameSpacedString @ZenGetter(value = "tagName") get() = this.tagIngredient.tag.name.toZen()
 
@@ -48,18 +45,7 @@ class ZenTagIngredient(val tagIngredient: TagIngredient, private val mark: Strin
     override fun or(ingredient: IIngredient?): IIngredient = IngredientOr(this, ingredient)
     override fun matches(item: IItemStack?): Boolean = this.contains(item) && this.conditions.all { it.matches(item) }
     override fun matches(liquid: ILiquidStack?): Boolean = false
-/*
-    override fun contains(item: IItemStack?): Boolean = this.tagIngredient.apply(item.toNativeStack())
-    override fun addAll(entry: IOreDictEntry?) = CraftTweakerAPI.logError("Unable to manipulate a tag when not in the 'tags' loader")
-    override fun getName(): String = this.tagIngredient.tag.name.toString()
-    override fun getFirstItem(): IItemStack? = this.tagIngredient.tag.elements.firstOrNull()?.let { it.toZen(wildcard = it.metadata == OreDictionary.WILDCARD_VALUE) }
-    override fun mirror(other: IOreDictEntry?) = CraftTweakerAPI.logError("Mirroring is not supported by tags")
-    override fun add(vararg items: IItemStack?) = CraftTweakerAPI.logError("Unable to manipulate a tag when not in the 'tags' loader")
-    override fun isEmpty(): Boolean = this.tagIngredient.tag.elements.isEmpty()
-    override fun remove(vararg items: IItemStack?) = CraftTweakerAPI.logError("Unable to manipulate a tag when not in the 'tags' loader")
-    override fun removeItems(items: Array<out IItemStack>?) = CraftTweakerAPI.logError("Unable to manipulate a tag when not in the 'tags' loader")
-    override fun addItems(items: Array<out IItemStack>?) = CraftTweakerAPI.logError("Unable to manipulate a tag when not in the 'tags' loader")
-*/
+
     private fun copy(mark: String? = this.mark, conditions: List<IItemCondition> = this.conditions, newTransformers: List<IItemTransformerNew> = this.newTransformers,
                      transformers: List<IItemTransformer> = this.transformers) = ZenTagIngredient(this.tagIngredient, mark, conditions, newTransformers, transformers)
 
