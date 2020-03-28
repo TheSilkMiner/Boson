2e59b38171a7a9cd9e82ba0acdddece0af29441d
TheSilkMiner <thesilkminer@outlook.com>
2020-03-28 09:42:04 +0100

    License pass
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/kotlin/net/thesilkminer/mc/boson/Boson.kt
+++ src/main/kotlin/net/thesilkminer/mc/boson/Boson.kt
@@ -1,3 +1,25 @@
+/*
+ * Copyright (C) 2020  TheSilkMiner
+ *
+ * This file is part of Boson.
+ *
+ * Boson is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Boson is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Boson.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Contact information:
+ * E-mail: thesilkminer <at> outlook <dot> com
+ */
+
 package net.thesilkminer.mc.boson
 
 import net.minecraftforge.fml.common.FMLCommonHandler
