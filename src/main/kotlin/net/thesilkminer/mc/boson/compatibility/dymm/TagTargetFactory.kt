c2b25654eff7036c62bb13bd3dcc75994cbf24a5
TheSilkMiner <thesilkminer@outlook.com>
2020-04-12 14:11:02 +0200

    Fix missing handling for initial dash in tag names
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/kotlin/net/thesilkminer/mc/boson/compatibility/dymm/TagTargetFactory.kt
+++ src/main/kotlin/net/thesilkminer/mc/boson/compatibility/dymm/TagTargetFactory.kt
@@ -6,6 +6,7 @@ import com.aaronhowser1.dymm.api.loading.GlobalLoadingState
 import com.aaronhowser1.dymm.api.loading.factory.TargetFactory
 import com.aaronhowser1.dymm.module.base.BasicItemTarget
 import com.google.gson.JsonObject
+import com.google.gson.JsonSyntaxException
 import net.thesilkminer.mc.boson.api.bosonApi
 import net.thesilkminer.mc.boson.prefab.naming.toNameSpacedString
 import net.thesilkminer.mc.boson.prefab.tag.itemTagType
@@ -13,8 +14,10 @@ import net.thesilkminer.mc.boson.prefab.tag.itemTagType
 @Suppress("unused")
 class TagTargetFactory : TargetFactory {
     override fun fromJson(state: GlobalLoadingState, `object`: JsonObject): List<Target> {
-        val tagName = JsonUtilities.getString(`object`, "tag").toNameSpacedString()
+        val tagName = JsonUtilities.getString(`object`, "tag").normalize().toNameSpacedString()
         val tag = bosonApi.tagRegistry[itemTagType, tagName]
         return tag.elements.map { BasicItemTarget(it) }.toList()
     }
+
+    private fun String.normalize() = if (!this.startsWith("#")) throw JsonSyntaxException("For string '$this': tag name must begin with '#'") else this.removePrefix("#")
 }
