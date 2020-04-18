d8f7ea352e60b9c05d2c922cb37b72a94c3f79d0
TheSilkMiner <thesilkminer@outlook.com>
2020-04-18 12:16:07 +0200

    Add empty JEI plugin for future compatibility if needed
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

---
+++ src/main/kotlin/net/thesilkminer/mc/boson/compatibility/jei/BosonJeiPlugin.kt
@@ -0,0 +1,10 @@
+package net.thesilkminer.mc.boson.compatibility.jei
+
+import mezz.jei.api.IModPlugin
+import mezz.jei.api.JEIPlugin
+
+@JEIPlugin
+@Suppress("unused")
+class BosonJeiPlugin : IModPlugin {
+
+}
---
+++ src/main/kotlin/net/thesilkminer/mc/boson/compatibility/jei/JustEnoughItemsCompatibilityProvider.kt
@@ -0,0 +1,7 @@
+package net.thesilkminer.mc.boson.compatibility.jei
+
+import net.thesilkminer.mc.boson.api.modid.JUST_ENOUGH_ITEMS
+import net.thesilkminer.mc.boson.compatibility.BosonCompatibilityProvider
+import net.thesilkminer.mc.boson.prefab.compatibility.ModCompatibilityProvider
+
+class JustEnoughItemsCompatibilityProvider : ModCompatibilityProvider(JUST_ENOUGH_ITEMS), BosonCompatibilityProvider
