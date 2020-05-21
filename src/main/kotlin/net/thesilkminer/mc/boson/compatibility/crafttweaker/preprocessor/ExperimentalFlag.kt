1d6fc5b44f2f5ab1da225ef5c55735cc31d8de51
TheSilkMiner <thesilkminer@outlook.com>
2020-05-21 14:19:00 +0200

    Add `-Esao` flag
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/kotlin/net/thesilkminer/mc/boson/compatibility/crafttweaker/preprocessor/ExperimentalFlag.kt
+++ src/main/kotlin/net/thesilkminer/mc/boson/compatibility/crafttweaker/preprocessor/ExperimentalFlag.kt
@@ -24,5 +24,6 @@ package net.thesilkminer.mc.boson.compatibility.crafttweaker.preprocessor
 
 internal enum class ExperimentalFlag(internal val flagName: String, internal val isDeprecated: Boolean = false) {
     SEQUENCE_OPERATOR_OVERLOADING("-Esoo"),
-    SEQUENCE_AUTOMATIC_IT_ARGUMENT("-Esaia")
+    SEQUENCE_AUTOMATIC_IT_ARGUMENT("-Esaia"),
+    SEQUENCE_ARRAY_ARGUMENT_OBTAINING("-Esao")
 }
